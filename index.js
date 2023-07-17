efd8e38ca2ede1d151529e8258ec83ff72e82ef8
Darian Nocera <darnocer@gmail.com>
2023-07-17 15:04:16 -0600

    hosted data api for gh-pages

--- index.js
+++ index.js
@@ -19,7 +19,8 @@ document.addEventListener("DOMContentLoaded", (event) => {
 
   // Fetch JSON data
   function getData() {
-    fetch("data.json")
+    // fetch("data.json")
+    fetch("https://darnocer.github.io/lunch/data.json")
       .then((response) => {
         if (!response.ok) {
           throw new Error("HTTP error " + response.status);
