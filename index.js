57fb36a4af33f802e248ec79540e6e6543c25187
Darian Nocera <darnocer@gmail.com>
2023-07-17 14:59:26 -0600

    update relative filepath for data.json

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ document.addEventListener("DOMContentLoaded", (event) => {
 
   // Fetch JSON data
   function getData() {
-    fetch("/data.json")
+    fetch("data.json")
       .then((response) => {
         if (!response.ok) {
           throw new Error("HTTP error " + response.status);
